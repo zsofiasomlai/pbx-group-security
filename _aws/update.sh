df4b8b7fef31ac448630886a24f225e08313dc46
Dinis Cruz <dinis.cruz@owasp.org>
2017-12-06 16:29:30 +0000

    Couple fixes

--- _aws/update.sh
+++ _aws/update.sh
@@ -5,4 +5,4 @@ aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --exclu
 
 DISTRIBUTION_ID=E3K53GHHL4D0LE
 #aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/*
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/*
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/*
