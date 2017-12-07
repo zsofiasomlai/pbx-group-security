0ce3ae7cbcc0099c27214bf63d8905509ae2da57
Dinis Cruz <dinis.cruz@owasp.org>
2017-12-07 15:55:42 +0000

    adding s3-sync

---
+++ _aws/s3-sync.sh
@@ -0,0 +1,5 @@
+BUCKET_NAME="pbx-group-security.com-cdn"
+DISTRIBUTION_ID=E3K53GHHL4D0LE
+
+aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --exclude 'post'
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/*
