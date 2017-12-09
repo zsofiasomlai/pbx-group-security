6d3f0ea0ef57ba4fa26f10769c053a4b8728ec03
Dinis Cruz <dinis.cruz@owasp.org>
2017-12-09 15:52:40 +0000

    updating live-reload branch

--- bin/start-servers.sh
+++ bin/start-servers.sh
@@ -1,2 +1,9 @@
 echo "Starting both servers"
-coffee src/start-server.coffee
+
+coffee src/start-server.coffee &
+
+cd ../../bin
+cd ../hugo-worktree master
+hugo
+cd public
+browser-sync start --server --no-open
