e789b35dc01db9a7c3e5c34e8540ef9ef3fcf52e
Dinis Cruz <dinis.cruz@owasp.org>
2017-12-17 15:11:00 +0000

    Renamed blog entries

--- _aws/update.sh
+++ _aws/update.sh
@@ -1,7 +1,7 @@
 BUCKET_NAME="pbx-group-security.com-cdn"
 
 hugo
-aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --exclude 'post'
+aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --delete
 
 DISTRIBUTION_ID=E3K53GHHL4D0LE
 #aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/*
