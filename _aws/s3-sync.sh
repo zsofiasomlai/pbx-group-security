afed63e744d175151e3b2a94cb61dc8a728664e3
Dinis Cruz <dinis.cruz@owasp.org>
2018-01-06 04:29:16 +0000

    commit for github push

--- _aws/s3-sync.sh
+++ _aws/s3-sync.sh
@@ -1,5 +1,5 @@
 BUCKET_NAME="pbx-group-security.com-cdn"
 DISTRIBUTION_ID=E3K53GHHL4D0LE
 
-aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --exclude 'post'
+aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --delete
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/* /contact/* /challenges/* /roles/* /77e653f4/ /77e653*
