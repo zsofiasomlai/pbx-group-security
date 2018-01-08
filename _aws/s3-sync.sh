d2e8fb7b75f1ee46d82ca419f04ac5941250fd22
Dinis Cruz <dinis.cruz@owasp.org>
2018-01-08 10:50:33 +0000

    commit for github push

--- _aws/s3-sync.sh
+++ _aws/s3-sync.sh
@@ -1,5 +1,9 @@
 BUCKET_NAME="pbx-group-security.com-cdn"
 DISTRIBUTION_ID=E3K53GHHL4D0LE
 
-aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --delete
+
+# --delete is having a nasty side effect to bring the side down when
+# aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --delete
+
+aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/* /contact/* /challenges/* /roles/* /77e653f4/ /77e653*
