e2da9bc82b43d356b0b31a4b02d774ec8f9c0a1e
Dinis Cruz <dinis.cruz@owasp.org>
2018-01-12 17:35:02 +0000

    commit for github push

--- bin/s3-sync.sh
+++ bin/s3-sync.sh
@@ -6,4 +6,4 @@ DISTRIBUTION_ID=E3K53GHHL4D0LE
 # aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --delete
 
 aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/* /contact/* /challenges/* /roles/* /recruiters/*
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/* /css/* /blog/* /contact/* /challenges/* /roles/* /recruiters/* /8d0d7c*
